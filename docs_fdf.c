e02a7f2c9a50b5e9f1a1d47ec9c57d645d49272b
Ahmed Salem <ahsalem@lab1r3s7.42abudhabi.ae>
2022-08-05 19:55:52 +0400

    quick wins, now I know what Bresenham's is

---
+++ docs_fdf.c
@@ -0,0 +1,12 @@
+                                                                        #Bism Ellah Elrahman Elraheem
+//This is the documentation for doing the fdf journey.
+//what I understood from the project now is the following:
+1-Get a map as Matrix of numbers
+2-Parse it to pointer of pointers
+3-Then go thorgh it item by item to put it as a pixel on an Image
+4-Hold on
+5-I believe you should do some calculations first to put it on a new matrix after some math
+6-Put them as pixels on an Image
+7-Present it using Minilibx
+8-Make sure that the image is smooth(Responsive, still the same upon changing the size of the window)
+9-ESC || (x)  close the window and free() everything 
--- project_code/open_window.c
+++ project_code/open_window.c
@@ -87,7 +87,7 @@ int	main(void)
 	mlx_put_image_to_window(mlx, mlx_window, img.img, 0, 0);
 	//mlx_key_hook(vars.win, key_hook, &vars);
 	mlx_mouse_hook(vars.win, mouse_hook, &vars);
-	
+	//mlx_mouse_get_pos(vars.win, 200, 1000);
 	//mlx_hook(vars.win, 2, 1L << 0, close, &vars);
 	mlx_loop(mlx);
 }
@@ -99,3 +99,4 @@ void my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel /8));
 	*(unsigned int*)dst = color;
 }
+
